0f345bb5247afd9412e6bd1a2118a824df27b970
excaliborr <excalibor@defi.sucks>
2024-02-06 14:15:35 -0500

    chore: config type

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-ethers';
-import '@defi-wonderland/sponsored-txs-hardhat-plugin';
+import '@defi-wonderland/hardhat-account-abstraction';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.19',
@@ -10,10 +10,9 @@ const config: HardhatUserConfig = {
     goerli: {
       url: process.env.RPC_URL as string,
       accounts: [process.env.PRIVATE_KEY as string],
-      sponsoredTransactions: {
+      accountAbstraction: {
         bundlerUrl: process.env.BUNDLER_URL as string,
         paymasterUrl: process.env.PAYMASTER_URL as string,
-        paymasterType: process.env.PAYMASTER_TYPE as string,
       }
     }
   }
